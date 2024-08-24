653b62876d67b76bdff96df4c2b2cb302d7365d5
Mouad Bouras <mboura@aseq.com>
2024-08-23 22:44:49 -0400

    switch up animation

--- src/components/animated-logo.tsx
+++ src/components/animated-logo.tsx
@@ -103,23 +103,23 @@ export default function AnimatedLogo() {
                     className="inline-block"
                     initial={{ opacity: 0, x: -100 }}
                     animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.2 }}
+                    transition={{ duration: 0.3, delay: 0.25 }}
                   >
                     CONTENT
                   </motion.span>{" "}
                   <motion.span
                     className="inline-block"
-                    initial={{ opacity: 0, x: -100 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.2, delay: 0.25 }}
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.3, delay: 0.25 }}
                   >
                     CREATOR
                   </motion.span>{" "}
                   <motion.span
                     className="inline-block"
-                    initial={{ opacity: 0, x: -100 }}
+                    initial={{ opacity: 0, x: 100 }}
                     animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.2, delay: 0.5 }}
+                    transition={{ duration: 0.3, delay: 0.25 }}
                   >
                     CIRCLES
                   </motion.span>
